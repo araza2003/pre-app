acc51662799b532d1dfa976f3c8b428d20006f29
araza2003 <ahmedrazaspan@gmail.com>
2025-02-09 21:01:26 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="nb-4">   {/*nb-4 = 16 pixils */}
+        Hello World!
+      </h1>
+      <p>Ahmed Raza</p>
+    </>
+  )
+}
+
+export default App
